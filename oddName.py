74233dbe49a39cdc267a40a525e1b14be949b897
CDivyaPrabha <31094031+CDivyaPrabha@users.noreply.github.com>
2018-03-28 17:31:57 +0800

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,11 @@
 """Divya Prabha Chandrasekaran"""
+flag = 0
+while flag == 0:
+    name = input("Enter your name: ")
+    for char in name:
+        if char.isupper() or char.islower():
+            flag = 1
+            break
+    if flag == 0:
+        print("Incorrect input")
+print(name[0:len(name):2])
